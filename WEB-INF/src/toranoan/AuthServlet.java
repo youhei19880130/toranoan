6b0b63b5f5837a79eb27dd5bce58d9951fa3cf17
yohhei someya <youhei19880130@yahoo.co.jp>
2014-07-16 14:22:38 +0900

    Merge branch 'master' of https://github.com/youhei19880130/toranoan
    Conflicts:
    	work/org/apache/jsp/login_jsp.class

--- WEB-INF/src/toranoan/AuthServlet.java
+++ WEB-INF/src/toranoan/AuthServlet.java
@@ -14,11 +14,11 @@ public class AuthServlet extends HttpServlet {
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws IOException, ServletException{
-		String authid = request.getParameter("authid");
-		String authpass = request.getParameter("authpass");
+		String entryid = request.getParameter("entryid");
+		String entrypass = request.getParameter("entrypass");
 		HttpSession session = request.getSession(true);
-		boolean check = AuthDao.authUser(authid, authpass);
-		if (check){
+		String correctpass = AuthDao.returnCorrectPass(entryid, entrypass);
+		if (entrypass.equals(correctpass)){
 			/* 認証済みにセットし、本来のアクセス先へ飛ばす */
 			session.setAttribute("login", "OK");
 			String target = "/toranoan/index.jsp";
@@ -27,6 +27,22 @@ public class AuthServlet extends HttpServlet {
 			/* 認証に失敗したら、ログイン画面に戻す */
 			session.setAttribute("status", "Not Auth");
 		    response.sendRedirect("/toranoan/login.jsp");
+		}
+	}
+	
+	String loginAuth (boolean check) {
+		HttpSession session = request.getSession(false);
+		String target = "/toranoan/login.jsp";
+		if (check){
+			/* 認証済みにセットし、本来のアクセス先へ飛ばす */
+			session.setAttribute("login", "OK");
+			target = "/toranoan/index.jsp";
+			return target;
+//			response.sendRedirect(target);
+		}else{
+			/* 認証に失敗したら、ログイン画面に戻す */
+			session.setAttribute("status", "Not Auth");
+			return target;
 		}
 	}
 	
